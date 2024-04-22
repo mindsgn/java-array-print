986b68114edbcfa20d0108818c794163fdb65864
mindsgn <zookid@protonmail.com>
2024-04-22 12:31:43 +0200

    array change

--- StringArrayExample.java
+++ StringArrayExample.java
@@ -2,15 +2,15 @@ public class StringArrayExample {
 
   public static void main(String[] args) {
     // Declare and initialize the string array
-    String[] fruits = {"one", "two", "three", "four", "five"};
+    String[] numbersArray = {"one", "two", "three", "four", "five"};
 
     // Loop through the array and print each element
-    for (int i = 0; i < fruits.length; i++) {
-      System.out.println(fruits[i]);
+    for (int i = 0; i < numbersArray.length; i++) {
+      System.out.println(numbersArray[i]);
     }
 
     // Determine and print the length of the array
-    int arrayLength = fruits.length;
+    int arrayLength = numbersArray.length;
     System.out.println("The length of the array is: " + arrayLength);
   }
 }
